ca67070393d5a9931bcc70243bd34a151eeddcf3
yanjunzheng <yanjunzheng@pset.suntec.net>
2013-10-15 14:22:57 +0800

    auto login

--- assistant/js/login.js
+++ assistant/js/login.js
@@ -1,32 +1,25 @@
 $(document).ready(function(){ 
-    var username = "";
-    var password = "";
-
-    var options = { 
-        url:'login.aspx', 
-        type:'POST', 
-        
-        success: function(){} 
-    };
+    var username = null;
+    var password = null;
 
     username = localStorage.getItem('username');
     password = localStorage.getItem('password');
 
-    //$('#form1').ajaxSubmit(options); 
-
     if(username != null && password != null){
         $('#tbUserName').val(username);
         $('#tbPassword').val(password);
-        //
+        $('#btnLogin').trigger("click");
     }
 
     $('#btnLogin').click(function(){
         username = $('#tbUserName').val();
         password = $('#tbPassword').val();
 
-        localStorage.setItem('username', username);
-        localStorage.setItem('password', password);
-        //$('#form1').ajaxSubmit(options); 
+        if(username != "" && password != "")
+        {
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+        }   
     });
 
 }); 
